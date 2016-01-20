c5e3b5798ff0ae3671302bba282f9d742516b8be
Simplelegant <466033631@qq.com>
2016-01-20 11:35:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/simpleelegant/activitylc/Aty1.java
@@ -0,0 +1,17 @@
+package com.example.simpleelegant.activitylc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Administrator on 2016/1/20/0020.
+ */
+public class Aty1 extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.aty1);
+    }
+}
