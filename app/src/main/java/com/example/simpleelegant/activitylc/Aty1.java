950103c669dbef19dac7d047c9efae95a1f78931
Simplelegant <466033631@qq.com>
2016-01-21 17:17:00 +0800

    just fun!!

--- app/src/main/java/com/example/simpleelegant/activitylc/Aty1.java
+++ app/src/main/java/com/example/simpleelegant/activitylc/Aty1.java
@@ -1,17 +1,42 @@
 package com.example.simpleelegant.activitylc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2016/1/20/0020.
  */
 public class Aty1 extends Activity{
+
+    private Button btnClose;
+    private TextView tvOut;
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.aty1);
+
+        btnClose = (Button)findViewById(R.id.btnclose);
+        btnClose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent();
+                i.putExtra("result", "Because i coded this program!");
+                setResult(0, i);
+                finish();
+            }
+        });
+
+        tvOut = (TextView)findViewById(R.id.tvout);
+        //tvOut.setText(getIntent().getStringExtra("txt"));
+
+        Bundle data = getIntent().getExtras();
+        String txt = data.getString("txt");
+        tvOut.setText(txt);
     }
 }
--- app/src/main/java/com/example/simpleelegant/activitylc/MainActivity.java
+++ app/src/main/java/com/example/simpleelegant/activitylc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.simpleelegant.activitylc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnStartAty1;
+    private TextView tvOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        tvOut = (TextView)findViewById(R.id.tvOut);
+        btnStartAty1 = (Button)findViewById(R.id.btnStartAty1);
+        btnStartAty1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, Aty1.class);
+                //i.putExtra("txt", "Hello Aty1");
+
+                Bundle data = new Bundle();
+                data.putString("txt", "没有");
+                i.putExtras(data);
+
+                //startActivity(i);
+                startActivityForResult(i, 0);
             }
         });
     }
@@ -48,5 +70,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        String result = data.getStringExtra("result");
+        tvOut.setText(result);
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
